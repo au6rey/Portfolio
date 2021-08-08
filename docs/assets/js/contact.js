e5ab0a7468d029ac0f3771888dc7cf29fcf45290
Aubrey <60026978+au6rey@users.noreply.github.com>
2021-08-08 10:26:45 -0500

    Final

--- docs/assets/js/contact.js
+++ docs/assets/js/contact.js
@@ -9,74 +9,63 @@ $(function () {
       var subject = $("input#subject").val();
       var message = $("textarea#message").val();
 
-      // var data = {
-      //   service_id: "service_88lb8rs",
-      //   template_id: "template_w95566d",
-      //   user_id: "user_KuGTDa4ByzuG91N2OW7pr",
-      //   template_params: {
-      //     from_name: name,
-      //     to_name: "Aubrey",
-      //     message: message,
-      //     subject: subject,
-      //     reply_to: email,
-      //     "g-recaptcha-response": capres,
-      //   },
-      // };
-      // var templateParams = {
-      //   from_name: name,
-      //   to_name: "Aubrey",
-      //   message: message,
-      //   subject: subject,
-      //   reply_to: email,
-      //   "g-recaptcha-response": capres,
-      // }
       $.ajax({
         cache: false,
         success: function () {
           // console.log(grecaptcha.getResponse());
-          emailjs
-            .send("service_88lb8rs", "template_w95566d", {
+
+          var data = {
+            service_id: "service_88lb8rs",
+            template_id: "template_w95566d",
+            user_id: "user_KuGTDa4ByzuG91N2OW7pr",
+            accessToken: "2dd7d8cff2f758d2a94e59982235a445",
+            template_params: {
               from_name: name,
               to_name: "Aubrey",
               message: message,
               subject: subject,
               reply_to: email,
-              "g-recaptcha-response": grecaptcha.getResponse(),
+            },
+          };
+
+          $.ajax("https://api.emailjs.com/api/v1.0/email/send", {
+            type: "POST",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+          })
+            .done(function () {
+              alert("Your mail is sent!");
+              $("#success").html("<div class='alert alert-success'>");
+              $("#success > .alert-success")
+                .html(
+                  "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                )
+                .append("</button>");
+              $("#success > .alert-success").append(
+                "<strong>Your message has been sent. </strong>"
+              );
+              $("#success > .alert-success").append("</div>");
+              $("#contactForm").trigger("reset");
             })
-            .then(
-              function (response) {
-                console.log("SUCCESS!", response.status, response.text);
-                $("#success").html("<div class='alert alert-success'>");
-                $("#success > .alert-success")
-                  .html(
-                    "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                  )
-                  .append("</button>");
-                $("#success > .alert-success").append(
-                  "<strong>Your message has been sent. </strong>"
-                );
-                $("#success > .alert-success").append("</div>");
-                $("#contactForm").trigger("reset");
-              },
-              function (error) {
-                console.log("FAILED...", error);
-                $("#success").html("<div class='alert alert-danger'>");
-                $("#success > .alert-danger")
-                  .html(
-                    "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                  )
-                  .append("</button>");
-                $("#success > .alert-danger").append(
-                  $("<strong>").text(
-                    "Sorry " +
-                      name +
-                      ", we could not send the email. Please try again later!"
-                  )
-                );
-                $("#success > .alert-danger").append("</div>");
-                $("#contactForm").trigger("reset");
-              }
-            );
+            .fail(function (error) {
+              alert("Oops... " + JSON.stringify(error));
+              console.log(JSON.stringify(error));
+              $("#success").html("<div class='alert alert-danger'>");
+              $("#success > .alert-danger")
+                .html(
+                  "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                )
+                .append("</button>");
+              $("#success > .alert-danger").append(
+                $("<strong>").text(
+                  "Sorry " +
+                    name +
+                    ", we could not send the email. Please try again later!"
+                )
+              );
+              $("#success > .alert-danger").append("</div>");
+              $("#contactForm").trigger("reset");
+            });
         },
         error: function () {
           $("#success").html("<div class='alert alert-danger'>");
