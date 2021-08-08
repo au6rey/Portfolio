44d4f0183cd55b0e042e6ad07fed69e882136fc1
Aubrey <60026978+au6rey@users.noreply.github.com>
2021-08-08 09:50:54 -0500

    EmailJS

--- docs/assets/js/contact.js
+++ docs/assets/js/contact.js
@@ -8,73 +8,99 @@ $(function () {
       var email = $("input#email").val();
       var subject = $("input#subject").val();
       var message = $("textarea#message").val();
-      var capres = grecaptcha.getResponse();
-      console.log(capres);
-      var data = {
-        service_id: "service_88lb8rs",
-        template_id: "template_w95566d",
-        user_id: "user_KuGTDa4ByzuG91N2OW7pr",
-        template_params: {
-          from_name: name,
-          to_name: "Aubrey",
-          message: message,
-          subject: subject,
-          reply_to: email,
-          "g-recaptcha-response": capres,
+
+      // var data = {
+      //   service_id: "service_88lb8rs",
+      //   template_id: "template_w95566d",
+      //   user_id: "user_KuGTDa4ByzuG91N2OW7pr",
+      //   template_params: {
+      //     from_name: name,
+      //     to_name: "Aubrey",
+      //     message: message,
+      //     subject: subject,
+      //     reply_to: email,
+      //     "g-recaptcha-response": capres,
+      //   },
+      // };
+      // var templateParams = {
+      //   from_name: name,
+      //   to_name: "Aubrey",
+      //   message: message,
+      //   subject: subject,
+      //   reply_to: email,
+      //   "g-recaptcha-response": capres,
+      // }
+      $.ajax({
+        cache: false,
+        success: function () {
+          grecaptcha.getResponse();
+          emailjs
+            .send("service_88lb8rs", "template_w95566d", {
+              from_name: name,
+              to_name: "Aubrey",
+              message: message,
+              subject: subject,
+              reply_to: email,
+              // "g-recaptcha-response": grecaptcha.getResponse(),
+            })
+            .then(
+              function (response) {
+                console.log("SUCCESS!", response.status, response.text);
+                $("#success").html("<div class='alert alert-success'>");
+                $("#success > .alert-success")
+                  .html(
+                    "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                  )
+                  .append("</button>");
+                $("#success > .alert-success").append(
+                  "<strong>Your message has been sent. </strong>"
+                );
+                $("#success > .alert-success").append("</div>");
+                $("#contactForm").trigger("reset");
+              },
+              function (error) {
+                console.log("FAILED...", error);
+                $("#success").html("<div class='alert alert-danger'>");
+                $("#success > .alert-danger")
+                  .html(
+                    "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                  )
+                  .append("</button>");
+                $("#success > .alert-danger").append(
+                  $("<strong>").text(
+                    "Sorry " +
+                      name +
+                      ", we could not send the email. Please try again later!"
+                  )
+                );
+                $("#success > .alert-danger").append("</div>");
+                $("#contactForm").trigger("reset");
+              }
+            );
         },
-      };
-
-      $.ajax("https://api.emailjs.com/api/v1.0/email/send", {
-        type: "POST",
-        data: JSON.stringify(data),
-        contentType: "application/json",
-      })
-        .done(function () {
-          alert("Your mail is sent!");
-        })
-        .fail(function (error) {
-          alert("Oops... " + JSON.stringify(error));
-        })
-        .catch((error) => console.log(error));
-
-      // $.ajax({
-      //   cache: false,
-      //   success: function () {
-      //     $("#success").html("<div class='alert alert-success'>");
-      //     $("#success > .alert-success")
-      //       .html(
-      //         "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-      //       )
-      //       .append("</button>");
-      //     $("#success > .alert-success").append(
-      //       "<strong>Your message has been sent. </strong>"
-      //     );
-      //     $("#success > .alert-success").append("</div>");
-      //     $("#contactForm").trigger("reset");
-      //   },
-      //   error: function () {
-      //     $("#success").html("<div class='alert alert-danger'>");
-      //     $("#success > .alert-danger")
-      //       .html(
-      //         "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-      //       )
-      //       .append("</button>");
-      //     $("#success > .alert-danger").append(
-      //       $("<strong>").text(
-      //         "Sorry " +
-      //           name +
-      //           ", it seems that our mail server is not responding. Please try again later!"
-      //       )
-      //     );
-      //     $("#success > .alert-danger").append("</div>");
-      //     $("#contactForm").trigger("reset");
-      //   },
-      //   complete: function () {
-      //     setTimeout(function () {
-      //       $this.prop("disabled", false);
-      //     }, 1000);
-      //   },
-      // });
+        error: function () {
+          $("#success").html("<div class='alert alert-danger'>");
+          $("#success > .alert-danger")
+            .html(
+              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+            )
+            .append("</button>");
+          $("#success > .alert-danger").append(
+            $("<strong>").text(
+              "Sorry " +
+                name +
+                ", it seems that our mail server is not responding. Please try again later!"
+            )
+          );
+          $("#success > .alert-danger").append("</div>");
+          $("#contactForm").trigger("reset");
+        },
+        complete: function () {
+          setTimeout(function () {
+            $this.prop("disabled", false);
+          }, 1000);
+        },
+      });
     },
     filter: function () {
       return $(this).is(":visible");
