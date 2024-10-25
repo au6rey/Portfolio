adcdc8ac5967840add4e93e04710c4a3f3e0d176
Aubrey <60026978+au6rey@users.noreply.github.com>
2024-10-25 09:19:16 -0500

    Change deploy action

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // <=== enables static exports
-  basePath: "/Portfolio",
+  basePath: "/portfolio",
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
